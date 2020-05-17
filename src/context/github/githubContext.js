dd235b25856c94b3fb7008b05452672eaebadb57
Himanshu Rawat <himanshurawatrit@gmail.com>
2020-05-17 13:36:39 +0530

    checking new branch

--- src/context/github/githubContext.js
+++ src/context/github/githubContext.js
@@ -2,4 +2,8 @@ import { createContext } from 'react';
 
 const githubContext = createContext();
 
+function add() {
+	console.log('This is in the new Branch ContextAPI');
+}
+
 export default githubContext;
